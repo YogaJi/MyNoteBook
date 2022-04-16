987a654c75b2aa59a09b0e2c0583591b5ab39999
YogaJi <y.disturbia@gmail.com>
2022-04-16 10:44:33 +0800

    add styling and notebook page
    add the notebook list in the homepage, and add main.css stylesheet for the first page styling.

--- MyNoteBook/Pages/Index.cshtml.cs
+++ MyNoteBook/Pages/Index.cshtml.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using MyNoteBook.Data;
+using MyNoteBook.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,14 +14,32 @@ namespace MyNoteBook.Pages
     {
         private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        private readonly MyNoteBookContext db;
+
+        [FromForm]
+        public Notebook Notebook { get; set; }
+
+        public List<Notebook> NotebookList { get; set; }
+        public IndexModel(ILogger<IndexModel> logger, MyNoteBookContext dbContext)
         {
             _logger = logger;
+            db = dbContext;
         }
 
         public void OnGet()
         {
+            var query = db.Notebook;
+            NotebookList = query.ToList();
+        }
+        public void OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                db.Notebook.Add(Notebook);
+                db.SaveChanges();
+            }
 
+            NotebookList = db.Notebook.ToList();
         }
     }
 }
---
+++ MyNoteBook/Pages/NoteBookPage.cshtml.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MyNoteBook.Data;
+using MyNoteBook.Models;
+
+namespace MyNoteBook.Pages
+{
+    public class NoteBookPageModel : PageModel
+    {
+        private readonly MyNoteBookContext db;
+
+        public NoteBookPageModel(MyNoteBookContext dbContext)
+        {
+            db = dbContext;
+        }
+
+        [FromQuery]
+        public Notebook Notebook { get; set; }
+        public void OnGet(int id)
+        {
+            Notebook = db.Notebook.Include(notebook => notebook.Journals).FirstOrDefault(notebook => notebook.NotebookId == id);
+            /*Journal = db.Journal.FirstOrDefault(n => n.JournalId == id);*/
+        }
+    }
+}
